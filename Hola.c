b6ec81dcff3f86b833620eaa4be794497d1f781e
agolly <andreas.golly@gmx.de>
2023-09-06 22:59:52 +0200

    Hola Hola

--- Hola.c
+++ Hola.c
@@ -3,6 +3,6 @@
 
 int main(int argc, char **argv) {
 	printf("\n\t----------\n\tHello World!\n\t----------\n");
-	printf("\n\t ---------\n\tHello Freital as well\n\t----------\n\n");
+	printf("\n\t---------------------\n\tHello Freital as well\n\t---------------------\n\n");
 	return 0;
 }
