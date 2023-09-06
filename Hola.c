3eb2570682a2bef7e0a26f47506d7d103307a474
agolly <andreas.golly@gmx.de>
2023-09-07 01:17:52 +0200

    modify

--- Hola.c
+++ Hola.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	printf("\n\t----------\n\tHello World!\n\t----------\n");
+	printf("\n\t-------------\n\tHello World!\n\t-------------\n");
 	printf("\n\t---------------------\n\tHello Freital as well\n\t---------------------\n\n");
 	return 0;
 }
